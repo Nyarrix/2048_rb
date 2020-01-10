58344e11f1b072ca01c7cee5758e631d769c736f
Nyarrix <takap.walkingliberty@gmail.com>
2020-01-11 00:12:49 +0900

    game initialization

--- main.rb
+++ main.rb
@@ -1,23 +1,62 @@
 require 'matrix'
 
 def main
-  g = Nisenyoujuhachi.new
-  g.print_field
+	g = Nisenyoujuhachi.new
+	g.game_init
+	g.print_field
 end
 
 class Nisenyoujuhachi
+	FIELD_SIZE = 4
   def initialize
-    @field = Matrix.zero(4, 4)
+		@field = Array.new(4) { Array.new(4, 0) }
+		@r = Random.new
   end
 
   def print_field
-    for i in 0..@field.row_size - 1
-      for j in 0..@field.column_size - 1
-        print sprintf("%5d ", @field[i, j])
+    for i in 0..FIELD_SIZE - 1
+      for j in 0..FIELD_SIZE - 1
+				print sprintf("%5d ", @field[i][j])
+				# print @field[i, j]
       end
         puts ""
     end
   end
+
+	def game_init
+		2.times { place_number }
+	end
+
+	def place_number
+		refresh_zero_index_list
+		if @zero_index_list.size != 0
+			r = rand(@zero_index_list.size)
+			x = @zero_index_list[r][0]
+			y = @zero_index_list[r][1] 
+			puts("#{x} #{y}")
+
+			@field[x][y] = generate_4_or_2
+		end
+	end
+
+	def generate_4_or_2
+		if @r.rand(4) == 0 # FIXME: magic number
+			4
+		else
+			2
+		end
+	end
+
+	def refresh_zero_index_list
+		@zero_index_list = []
+    for i in 0..FIELD_SIZE - 1
+      for j in 0..FIELD_SIZE - 1
+				if @field[i][j] == 0
+					@zero_index_list.push([i, j])
+				end
+      end
+    end
+	end
 
   attr_accessor :field
 end
